f8c17be1615ed2998fe6b5b207df5781674213b6
DavidLee999 <MoonLee003@outlook.com>
2017-09-17 21:20:55 +0200

    xiaomi reverse sentence

---
+++ xiaomi_reverseSentence.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    string str, tmp;
+    while (cin >> tmp) {
+        str.append(tmp);
+        str.append(" ");
+    }
+
+    char* cstr = new char[str.length() + 1];
+    std::strcpy(cstr, str.c_str());
+
+    char* pch = strtok(cstr, " ");
+    
+    vector<string> res;
+    while (pch != NULL) {
+        string st { pch };
+        res.push_back(st);
+        pch = strtok(NULL, " ");
+    }
+
+    for (int i = res.size() - 1; i > 0; --i)
+        cout << res[i] << " ";
+    cout << res[0];
+
+    return 0;
+}
