0bf12bce1b243dcea79414ba2177928df59d0cc5
DavidLee999 <MoonLee003@outlook.com>
2017-09-17 10:29:09 +0200

    calculateMax

--- calculateMax.cpp
+++ calculateMax.cpp
@@ -1,52 +1,34 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
-#include <functional>
 using namespace std;
+
+int getMax(const vector<int>& prices, int start, int end)
+{
+    int MIN = prices[start];
+    int MAX = 0;
+    for (int i = start + 1; i <= end; ++i)
+    {
+        if (prices[i] < MIN)
+            MIN = prices[i];
+        else if (prices[i] - MIN > MAX)
+            MAX = prices[i] - MIN;
+    }
+
+    return MAX;
+}
+
 int calculateMax(vector<int> prices) 
 {
-    int length = prices.size();
-    vector<int> diff (length);
-    for (int i = 1; i < length; ++i)
-    {
-        diff[i - 1] = prices[i] - prices[i - 1];
-    }
-    
-    diff[length - 1] = -1;
+   int max = 0, tmp = 0;
 
-    vector<int> earned;
-    int n = 0;
-    for (int i = 0; i < diff.size(); ++i)
-    {
-        if (diff[i] >= 0) {
-            n += diff[i];
-        }
-        else {
-            if (n > 0)
-            {
-                earned.push_back(n);
-                n = 0;
-            }
-            else
-                continue;
-        }
-    }
-    
-    for (int i = 0; i < earned.size(); ++i)
-        cout << earned[i] << " ";
-    cout << endl;
+   for (int i = 1; i < prices.size(); ++i)
+   {
+       tmp = getMax(prices, 0, i) + getMax(prices, i, prices.size() - 1);
+       if (tmp > max)
+           max = tmp;
+   }
 
-    if (earned.empty())
-        return 0;
-    else if (earned.size() == 1)
-        return earned[0];
-    else if (earned.size() == 2)
-        return earned[0] + earned[1];
-    else
-    {
-        std::sort(earned.begin(), earned.end(), std::greater<int>());
-        return earned[0] + earned[1];
-    }
+   return max;
 }
 
 int main()
