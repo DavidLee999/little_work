46e9db95e5c93e88e0ea041fdf6d763127b4ccf8
DavidLee999 <MoonLee003@outlook.com>
2017-10-24 22:10:03 +0200

    li

--- leetcode/leetcode_matching.cpp
+++ leetcode/leetcode_matching.cpp
@@ -3,16 +3,18 @@
 using namespace std;
 
 
-bool isMatch(const string& s, const string& p)
+bool isMatch(const string& str1, const string& str2)
 {
     int i = 0, j = 0, star = -1;
     while(i < str1.size()) {
+        if (j > str2.size())
+            return false;
         if (str2[j] == '?' || str1[i] == str2[j])
         {
             ++i;
             ++j;
             star = -1;
-            continue;
+            // continue;
         }
 
         if (str2[j] == '*')
@@ -25,6 +27,7 @@ bool isMatch(const string& s, const string& p)
             j = star;
             ++i;
         }
+
     }
 
     if (j >= str2.size())
